15f8811820534a44a073d776774bd5d7946eaec5
Sanja <sanja1mandic@gmail.com>
2022-09-01 19:30:23 +0200

    add reverse and fizzbuzz methods

--- solver.rb
+++ solver.rb
@@ -1,3 +1,5 @@
+# rubocop:disable Style/Documentation
+
 class Solver
   def factorial(number)
     raise RangeError, "You can't put negative value as argument when calling factorial method" if number.negative?
@@ -5,5 +7,17 @@ class Solver
 
     number * factorial(number - 1)
   end
-  
+
+  def reverse(word)
+    word.reverse
+  end
+
+  def fizzbuzz(number)
+    if number.%(3).zero? && number.%(5).zero? then 'fizzbuzz'
+    elsif number.%(3).zero? then 'fizz'
+    elsif number.%(5).zero? then 'buzz'
+    else
+      number.to_s
+    end
+  end
 end
