51d061d9c15e190642b74391d90de77444634c90
Sanja <sanja1mandic@gmail.com>
2022-09-01 18:58:21 +0200

    Mkae class Solver and factorial method

--- solver.rb
+++ solver.rb
@@ -1,1 +1,9 @@
+class Solver
+  def factorial(number)
+    raise RangeError, "You can't put negative value as argument when calling factorial method" if number.negative?
+    return 1 if number.zero? || number == 1
 
+    number * factorial(number - 1)
+  end
+  
+end
