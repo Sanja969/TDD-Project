e54980bbf35389f1397fa7416588a8e7081f4779
Sanja <sanja1mandic@gmail.com>
2022-09-01 18:25:29 +0200

    Test faactorial method when argument is 0 or 1, and when it is negative

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -15,5 +15,13 @@ describe Solver do
     it 'returns 720 for factorial of 6' do
       expect(@solver).factorial(6).to eql 720
     end
+    it 'returns 1 for factorial 1 or 0' do
+      expect(@solver).factorial(0).to eql 1
+      expect(@solver).factorial(1).to eql 1
+    end
+    it 'throw RangeError exceptioon for factorial of negative value' do
+      expect(@solver).factorial(0).to eql 1
+      expect(@solver).factorial(1).to eql 1
+    end
   end
 end
