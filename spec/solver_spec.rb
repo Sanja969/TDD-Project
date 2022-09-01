1b3d1f678389a3a1bff281dcdb7b9e50443993b9
Sanja <sanja1mandic@gmail.com>
2022-09-01 18:45:17 +0200

    add Test for reverse and fizzbuzz method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -13,15 +13,35 @@ describe Solver do
 
   describe '#factorial' do
     it 'returns 720 for factorial of 6' do
-      expect(@solver).factorial(6).to eql 720
+      expect(@solver.factorial(6)).to eql 720
     end
     it 'returns 1 for factorial 1 or 0' do
-      expect(@solver).factorial(0).to eql 1
-      expect(@solver).factorial(1).to eql 1
+      expect(@solver.factorial(0)).to eql 1
+      expect(@solver.factorial(1)).to eql 1
     end
     it 'throw RangeError exceptioon for factorial of negative value' do
-      expect(@solver).factorial(-6).to raise_exception RangeError
+      expect(@solver.factorial(-6)).to raise_exception RangeError
+    end
+  end
+
+  describe '#reverse' do
+    it 'returns 720 for factorial of 6' do
+      expect(@solver.reverse('word')).to eql 'drow'
+    end
+  end
+
+  describe '#fizzbuzz' do
+    it 'when argument is divisible by 3' do
+      expect(@solver.fizzbuzz(6)).to eql 'fizz'
+    end
+    it 'when argument is divisible by 5' do
+      expect(@solver.fizzbuzz(10)).to eql 'buzz'
+    end
+    it 'when argument is divisible by 3 and 5' do
+      expect(@solver.fizzbuzz(15)).to eql 'fizzbuzz'
+    end
+    it 'when argument is not divisible by 3 or 5' do
+      expect(@solver.fizzbuzz(15)).to eql '15'
     end
   end
 end
-
