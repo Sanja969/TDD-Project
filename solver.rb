c34f204bb5e0cc4664f069933e222378f5b31f12
ABDULALI3468 <mrabdulali2003@gmail.com>
2022-09-02 15:13:10 +0500

    Added solver class!

--- solver.rb
+++ solver.rb
@@ -1,1 +1,21 @@
+class Solver
+  def factorial(number)
+    raise RangeError, "You can't put negative value as argument when calling factorial method" if number.negative?
+    return 1 if number.zero? || number == 1
 
+    number * factorial(number - 1)
+  end
+
+  def reverse(word)
+    word.reverse
+  end
+
+  def fizzbuzz(number)
+    if number.%(3).zero? && number.%(5).zero? then 'fizzbuzz'
+    elsif number.%(3).zero? then 'fizz'
+    elsif number.%(5).zero? then 'buzz'
+    else
+      number.to_s
+    end
+  end
+end
